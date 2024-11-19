88a21a37782595788099da5b15bc0ec57ec123fe
Michael Rogov <mihaillogin5@gmail.com>
2024-11-19 21:52:43 +0300

    добавление  FileOperationException

--- exceptions.py
+++ exceptions.py
@@ -19,3 +19,12 @@ class InvalidFormatException(AudioEditorException):
         self.format_type = format_type
         self.message = f"Invalid format: {format_type}."
         super().__init__(self.message)
+
+class FileOperationException(AudioEditorException):
+    """Ошибка при сохранении или загрузке файлов."""
+
+    def __init__(self, operation, filename):
+        self.operation = operation
+        self.filename = filename
+        self.message = f"Error {operation} file: {filename}."
+        super().__init__(self.message)
