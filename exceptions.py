8821e5cf9030fb4714ac3d0b77ae83fc7ab332d7
Michael Rogov <mihaillogin5@gmail.com>
2024-11-19 21:51:45 +0300

    добавление InvalidFormatException

--- exceptions.py
+++ exceptions.py
@@ -11,3 +11,11 @@ class TrackNotFoundException(AudioEditorException):
         self.track_id = track_id
         self.message = f"Трек с ID {track_id} не найден."
         super().__init__(self.message)
+
+class InvalidFormatException(AudioEditorException):
+    """Ошибка, возникающая при попытке сохранить или загрузить проект в неподдерживаемом формате."""
+
+    def __init__(self, format_type):
+        self.format_type = format_type
+        self.message = f"Invalid format: {format_type}."
+        super().__init__(self.message)
